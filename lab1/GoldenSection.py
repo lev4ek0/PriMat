117376cb3e200dd1883dd1ece937178328bf9aff
lev4ek0 <67754231+lev4ek0@users.noreply.github.com>
2021-03-27 16:49:04 +0300

    Update GoldenSection.py

--- lab1/GoldenSection.py
+++ lab1/GoldenSection.py
@@ -1,26 +1,23 @@
-import math
 import Const
 
 
 def x1(a1, b1):
-    return a1 + (math.sqrt(5) - 1) / 2 * (b1 - a1)
+    return (a1 + b1) / 2 - Const.e / 2
 
 
 def x2(a1, b1):
-    return b1 - (math.sqrt(5) - 1) / 2 * (b1 - a1)
+    return (a1 + b1) / 2 + Const.e / 2
 
 
-def golden_section():
+def dichotomy():
     A = Const.a
     B = Const.b
     counter1 = 0
     counter2 = 0
     while B - A > Const.e:
-        counter1 += 1
         X1 = x1(A, B)
         X2 = x2(A, B)
-        if X2 < X1:
-            X1, X2 = X2, X1
+        counter1 += 1
         Y1 = Const.f(X1)
         Y2 = Const.f(X2)
         counter2 += 2
@@ -31,4 +28,4 @@ def golden_section():
         else:
             A = X1
             B = X2
-    return [(A + B) / 2, counter1, counter2]
+    return {"Локальный минимум": (A + B) / 2, "Количество итераций": counter1, "Количество вычислений": counter2}
